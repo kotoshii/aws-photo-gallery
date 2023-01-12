a0601d8eee1e6a9306874410e3e1b9f39cfceddb
Oleksandr Semeniuk <o.semeniuk31@gmail.com>
2023-01-12 19:37:36 +0200

    Minor refactor.

--- src/store/slices/auth.slice.ts
+++ src/store/slices/auth.slice.ts
@@ -15,7 +15,7 @@ const initialState: AuthState = {};
 export const login = createAsyncThunk<LoginResponse, LoginRequest>(
   'auth/login',
   async ({ email, password }) => {
-    const resp = (await Auth.signIn(email, password)) as CognitoUser;
+    const resp: CognitoUser = await Auth.signIn(email, password);
     console.log(resp);
     return resp;
   },
@@ -24,11 +24,11 @@ export const login = createAsyncThunk<LoginResponse, LoginRequest>(
 export const signUp = createAsyncThunk<SignUpResponse, SignUpRequest>(
   'auth/login',
   async ({ email, name, password }) => {
-    const resp = (await Auth.signUp({
+    const resp: ISignUpResult = await Auth.signUp({
       username: email,
       password,
       attributes: { name },
-    })) as ISignUpResult;
+    });
     console.log(resp);
     return resp.user;
   },
