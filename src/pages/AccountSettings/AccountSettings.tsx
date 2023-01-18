8d8c4657309146990193698ef884fde45b1a14a6
pusuwa <>
2023-01-18 19:25:36 +0200

    Create account settings page layout.

--- src/pages/AccountSettings/AccountSettings.tsx
+++ src/pages/AccountSettings/AccountSettings.tsx
@@ -1,7 +1,56 @@
+/** @jsxImportSource @emotion/react */
 import React from 'react';
+import { Button, Divider, Grid, Paper, TextField } from '@mui/material';
+import {
+  accountSettings,
+  accountSettingsPage,
+  avatar,
+  divider,
+  uploadAvatarButton,
+} from './styles';
+import { UserAvatar } from '@components';
 
 function AccountSettings() {
-  return <div>AccountSettings</div>;
+  return (
+    <Grid container css={accountSettingsPage}>
+      <Grid item xs={12} sm={12} md={6} lg={6} xl={4} mx="auto">
+        <Paper elevation={0} css={accountSettings}>
+          <UserAvatar size={140} css={avatar} />
+          <Button variant="contained" disableElevation css={uploadAvatarButton}>
+            change profile pic
+          </Button>
+          <Divider flexItem css={divider} />
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField label="Name" fullWidth />
+            </Grid>
+            <Grid item xs={12}>
+              <Button variant="contained" disableElevation fullWidth>
+                save name
+              </Button>
+            </Grid>
+          </Grid>
+          <Divider flexItem css={divider} />
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField label="Old password" fullWidth />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField label="New password" fullWidth />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField label="Confirm new password" fullWidth />
+            </Grid>
+            <Grid item xs={12}>
+              <Button variant="contained" disableElevation fullWidth>
+                update password
+              </Button>
+            </Grid>
+          </Grid>
+        </Paper>
+      </Grid>
+    </Grid>
+  );
 }
 
 export default AccountSettings;
